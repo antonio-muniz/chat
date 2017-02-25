f7676939422366811e31a25515225c8a0e634fe2
antonio-muniz <ahnmhenrique@gmail.com>
2017-02-25 18:17:35 -0300

    :: Folder structure change for Heroku support :: - Moved client folder into the server folder; - Made server folder the root folder;

--- scripts/travis-install.sh
+++ scripts/travis-install.sh
@@ -3,11 +3,5 @@
 # -e: Exit on error, -v: print script lines before executing them
 set -ev
 
-# Enter server-side code folder
-cd server
-
 # Install dependencies
 npm install
-
-# Return to root folder
-cd ..
--- scripts/travis-script.sh
+++ scripts/travis-script.sh
@@ -3,14 +3,8 @@
 # -e: Exit on error, -v: print script lines before execute them
 set -ev
 
-# Enter server-side code folder
-cd server
-
 # Check code standards
 npm run lint
 
 # Run unit tests
 npm test
-
-# Return to root folder
-cd ..
