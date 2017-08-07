ebc6516acba1712d3b8892b9621f42af6cf86617
antonio-muniz <ahnmhenrique@gmail.com>
2017-08-06 23:12:14 -0300

    Fixed error definitions

--- app/errors.js
+++ app/errors.js
@@ -2,11 +2,7 @@
 
 module.exports = {
   USER_ALREADY_EXISTS: {
-    message: 'The specified user name already belongs to an active user',
-    statusCode: 400
-  },
-  UNEXPECTED_ERROR: {
-    message: 'An unexpected error has occurred while fulfilling the request',
-    statusCode: 500
+    status: 400,
+    message: 'The specified user name already belongs to an active user'
   }
 };
